c02c914103ef40ec7b09778008ece2189da6522c
mx-it-runner <mxpashkov@gmail.com>
2023-10-17 11:16:29 +0500

    add number list

--- app.py
+++ app.py
@@ -1,32 +1,33 @@
-import pandas as pd
-import openpyxl
+
 from flask import Flask, render_template, url_for, request, send_file
+import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-@app.route('/number')
-def cheknumber():
-    return render_template("number.html")
-
-@app.route('/upload', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        if 'file' not in request.files:
-            return 'Файл не найден.', 400
-
+        # Получение загруженного файла из запроса
         file = request.files['file']
 
+        # Проверка наличия файла
         if file.filename == '':
-            return 'Файл не выбран.', 400
+            return 'Файл не выбран'
 
-        file.save(file.filename)
-        return 'Файл успешно загружен.', 200
-    else:
-        return 'Для загрузки файлов разрешен только метод POST.'
+        # Проверка расширения файла
+        if file.filename.endswith('.xlsx'):
+            # Обрабатываем содержимое файла
+            numbers = []
+            try:
+                df = pd.read_excel(file)
+                numbers = df['Numbers'].tolist()
+                return str(numbers)
+            except Exception as e:
+                return f"Ошибка при обработке загруженного файла: {str(e)}"
+        else:
+            return 'Разрешены только файлы с расширением XLSX'
+
+    return render_template('upload.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
